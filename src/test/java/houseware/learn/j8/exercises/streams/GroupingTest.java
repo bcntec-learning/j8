b69d374be1126361350f313db3d76bb7db78a1ac
Francisco Philip <fphilip@houseware.es>
2015-10-21 15:59:00 +0200

    update

---
+++ src/test/java/houseware/learn/j8/exercises/streams/GroupingTest.java
@@ -0,0 +1,37 @@
+package houseware.learn.j8.exercises.streams;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static houseware.learn.j8.exercises.streams.Grouping.groupByNationality7;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author fphilip@houseware.es
+ */
+public class GroupingTest {
+
+    @Test
+    public void partitionAdultsShouldSeparateKidsFromAdults() {
+        Person sara = new Person("Sara", 4, "Norwegian");
+        Person viktor = new Person("Viktor", 40, "Serbian");
+        Person eva = new Person("Eva", 42, "Norwegian");
+        List<Person> collection = asList(sara, eva, viktor);
+        Map<String, List<Person>> result = groupByNationality7(collection);
+        assertThat(result.get("Norwegian")).hasSameElementsAs(asList(sara, eva));
+        assertThat(result.get("Serbian")).hasSameElementsAs(asList(viktor));
+    }
+
+    @Test
+    public void compare() {
+        Person sara = new Person("Sara", 4, "Norwegian");
+        Person viktor = new Person("Viktor", 40, "Serbian");
+        Person eva = new Person("Eva", 42, "Norwegian");
+        List<Person> collection = asList(sara, eva, viktor);
+//        assertThat(groupByNationality7(collection)).hasSameElementsAs(groupByNationality8(collection));
+    }
+
+}
---
+++ src/test/java/houseware/learn/j8/exercises/streams/Person.java
@@ -0,0 +1,38 @@
+package houseware.learn.j8.exercises.streams;
+
+/**
+ * @author fphilip@houseware.es
+ */
+public class Person {
+
+    public Person(final String nameValue, final int ageValue) {
+        name = nameValue;
+        age = ageValue;
+    }
+
+    public Person(final String nameValue, final int ageValue, final String nationalityValue) {
+        name = nameValue;
+        age = ageValue;
+        nationality = nationalityValue;
+    }
+
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    private String nationality;
+
+    public String getNationality() {
+        return nationality;
+    }
+
+}
