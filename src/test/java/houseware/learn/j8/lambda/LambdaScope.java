b08d68e94401e156c7e5066e56066883628c4e00
fphilip <fphilip@houseware.es>
2018-02-28 04:45:25 +0100

    scope

---
+++ src/test/java/houseware/learn/j8/lambda/LambdaScope.java
@@ -0,0 +1,37 @@
+package houseware.learn.j8.lambda;
+
+public class LambdaScope {
+
+    private String value = "Enclosing scope value";
+
+    public static void main(String[] args) {
+        String s = new LambdaScope().scopeExperiment();
+        System.out.println(s);
+    }
+
+    public String scopeExperiment() {
+        Foo fooIC = new Foo() {
+            String value = "Inner class value";
+
+            @Override
+            public String method(String string) {
+                return this.value;
+            }
+        };
+        String resultIC = fooIC.method("");
+
+        Foo fooLambda = parameter -> {
+            String value = "Lambda value";
+            return this.value;
+        };
+        String resultLambda = fooLambda.method("");
+
+        return "Results: resultIC = " + resultIC +
+                ", resultLambda = " + resultLambda;
+    }
+
+    @FunctionalInterface
+    public interface Foo {
+        String method(String string);
+    }
+}
